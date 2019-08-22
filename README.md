0f00da8bd6b02be764b29ee885c9d768a441284a
Hoàng Đình Tùng <tunghd@inhs-MacBook-Pro.local>
2019-08-22 16:08:44 +0700

    initialize

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
 # non-module-bundler-plugin
-A tiny plugin to help pack js files together with out using webpack bundler.
 
-This plugin will simply merge all your input js files into a single one
+<h1>Non Module Bundler Plugin</h1>
+<p>This plugin will simply merge all your input js files into a single one. Sorry, no obfuscate or minify, etc .. supported yet</p>
+<h2 align="center">Install</h2>
+```bash
+  npm i --save-dev non-module-bundler-plugin
+```
 
-Sorry, no obfuscate or minify, etc .. supported yet
+
+<h2 align="center">Usage</h2>
+
+Just add the plugin to your `webpack` config as follows:
+
+**webpack.config.js**
+```js
+const path = require('path');
+const NonModuleBundlerPlugin = require('non-module-bundler-plugin')
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js'
+  },
+  plugins: [
+    new NonModuleBundlerPlugin({
+            filename: 'libs.js',      // This is your output bundle name
+            sourceFiles: [
+                path.resolve(__dirname, './src/lib/script1.js'),       // paths to script files
+                path.resolve(__dirname, './src/lib/script2.js'),
+                path.resolve(__dirname, './src/script3.js'),
+                path.resolve(__dirname, './src/lib/america_lib/script4.js'),
+                path.resolve(__dirname, './src/lib/china_lib/script5.js')
+            ]
+        })
+  ]
+}
+```
+
+This will generate a file named `libs.js` in your build folder `dist` containing merged code of all your script files above
+
+
+<br>
+<br>
+
---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const promisify = require('util.promisify');
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
+const fsReadFileAsync = promisify(fs.readFile);
+
+class NonModuleBundlerPlugin {
+
+    constructor(options) {
+        // Default options
+        this.options = _.extend({
+            filename: 'libs.js',
+            sourceFiles: []
+        }, options);
+    }
+
+    apply(compiler) {
+        let self = this,
+            outputFileName = self.options.filename,
+            sourceFiles = self.options.sourceFiles;
+
+        // emit is asynchronous hook, tapping into it using tapAsync, you can use tapPromise/tap(synchronous) as well
+        compiler.hooks.emit.tapAsync('NonModuleBundlerPlugin', (compilation, callback) => {
+
+            if (!(sourceFiles instanceof Array)) {
+                console.error('Failed to read source files: must be an array of file path.');
+                return;
+            }
+
+            if (sourceFiles.length <= 0) {
+                callback();
+                return;
+            }
+
+            let outputContent = '',
+                promises = [];
+
+            for (let i = 0; i < sourceFiles.length; i++) {
+                let fname = String(sourceFiles[i]);
+                // console.log(fname + ' \n');
+                promises.push(fsReadFileAsync(fname));
+            }
+
+            Promise.all(promises)
+                .then(results => {
+                    for (let k = 0; k < results.length; k++) {
+                        outputContent += String(results[k]) + '\n\n';
+                    }
+
+                    compilation.assets[outputFileName] = {
+                        source: () => outputContent,
+                        size: () => outputContent.length
+                    };
+
+                    callback();
+                })
+                .catch(function(reason) {
+                    console.error(reason);
+                });
+        });
+    }
+
+}
+
+module.exports = NonModuleBundlerPlugin;
